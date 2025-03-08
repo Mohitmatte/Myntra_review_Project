ba6aa32c828e7cc073a251a164c91e80045856d0
Mohit_Matte <mattemohit2002@gmail.com>
2025-03-08 22:38:18 +0530

    This is my first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='Scrapper',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    author='Mohit',
+    author_email='mattemohit2002@gmail.com' 
+)
